97443cddbc1fe7c6e75b4b337f185b55af1ce746
yukunfeng <yukunfg@gmail.com>
2018-07-22 18:43:26 +0900

    translation dataset using torchtext

--- dataset.py
+++ dataset.py
@@ -10,11 +10,15 @@ Description : Dataset class using torchtext
 import torchtext
 
 
-def create_soccer_dataset(train_path, test_path, valid_path):
+def create_soccer_dataset(train_dir, test_dir, valid_dir):
     """create soccer dataset.
     :returns: iterators for train, test and valid dataset
 
     """
+    train_dir = train_dir + "/"
+    test_dir = test_dir + "/"
+    valid_dir = valid_dir + "/"
+
     def tokenize(events):
         """tokenize events"""
         return events.split()
@@ -22,42 +26,53 @@ def create_soccer_dataset(train_path, test_path, valid_path):
     EVENTS = torchtext.data.Field(
         sequential=True,
         tokenize=tokenize,
-        use_vocab=False
+        use_vocab=True,
+        lower=True
     )
 
     COMMENT = torchtext.data.Field(
         sequential=True,
         tokenize=tokenize,
-        use_vocab=False
+        use_vocab=True
     )
     soccer_fields = [("events", EVENTS), ("comment", COMMENT)]
-    train, test, valid = torchtext.data.TabularDataset.splits(
+    train, test, valid = torchtext.datasets.TranslationDataset.splits(
+        exts=("src", "tgt"),
+        fields=soccer_fields,
         path="",
-        train=train_path,
-        validation=valid_path,
-        test=test_path,
-        format='csv',
-        fields=soccer_fields
+        train=train_dir,
+        validation=valid_dir,
+        test=test_dir
     )
 
+    # Each Field only uses its own column to build vocab
     COMMENT.build_vocab(train)
+    EVENTS.build_vocab(train)
+    words = EVENTS.vocab.freqs.keys()
+    #  print(len(words))
 
-    train_iter, test_iter, val_iter = torchtext.data.BucketIterator.splits(
-        (train, test, valid),
-        batch_sizes=(2, 2, 2),
-        device=-1,
+    #  train_iter, val_iter = torchtext.data.Iterator.splits(
+    train_iter, val_iter = torchtext.data.BucketIterator.splits(
+        (train, valid),
+        batch_sizes=(2, 2),
+        device="cpu",
         sort_within_batch=False,
         sort_key=lambda x: len(x.comment),
         repeat=False
     )
+    test_iter = None
     
     return (train_iter, test_iter, val_iter)
 
 
 if __name__ == "__main__":
     #  unit test
-    create_soccer_dataset(
-        "./data/train.csv",
-        "./data/test.csv",
-        "./data/valid.csv"
+    data_dir = "./data_syn"
+    train_iter, test_iter, val_iter = create_soccer_dataset(
+        train_dir=f"./{data_dir}/train",
+        test_dir=f"./{data_dir}/test",
+        valid_dir=f"./{data_dir}/val"
     )
+
+    for train_batch in train_iter:
+        print(train_batch.comment)
--- train.py
+++ train.py
@@ -11,6 +11,8 @@ import argparse
 import opts
 from utils.utils import get_logger
 from onmt.decoders.decoder import StdRNNDecoder
+from onmt.encoders.encoder import RNNEncoder
+from onmt.models.model import NMTModel
 from torch import nn
 
 
@@ -28,10 +30,15 @@ def parse_args():
     return opt
 
 
-if __name__ == "__main__":
-    opt = parse_args()
-    logger = get_logger(opt.log_file)
-    logger.info("It's a test")
+def train(logger=None):
+    "training process"
+
+    encoder = RNNEncoder(
+        rnn_type="GRU", bidirectional=False,
+        num_layers=1, vocab_size=10,
+        word_dim=5, hidden_size=10
+    )
+
     decoder_emb = nn.Embedding(100, 20)
     decoder = StdRNNDecoder(
         rnn_type="GRU",
@@ -40,3 +47,16 @@ if __name__ == "__main__":
         hidden_size=30,
         embeddings=decoder_emb
     )
+
+    nmt = NMTModel(
+        encoder=encoder,
+        decoder=decoder,
+        multigpu=False
+    )
+
+
+if __name__ == "__main__":
+    #  opt = parse_args()
+    #  logger = get_logger(opt.log_file)
+    #  logger.info("It's a test")
+    train()
